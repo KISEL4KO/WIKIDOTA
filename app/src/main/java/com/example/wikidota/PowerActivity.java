37735be40ffb861aed5a980558e224c4a56df0de
roman <117850602+KISEL4KO@users.noreply.github.com>
2023-05-06 22:54:15 +0300

    1.2

--- app/src/main/java/com/example/wikidota/PowerActivity.java
+++ app/src/main/java/com/example/wikidota/PowerActivity.java
@@ -2,7 +2,11 @@ package com.example.wikidota;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.wikidota.characters.EnchantressActivity;
 
 public class PowerActivity extends AppCompatActivity {
 
@@ -11,4 +15,189 @@ public class PowerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_power);
     }
+    public void getEnchantress(View view){
+        Intent intent = new Intent(PowerActivity.this,  EnchantressActivity.class);
+        startActivity(intent);
+
+
+    }
 }
+
+/* шаблон для переходя на активности героев
+
+ public void get(View view){
+
+  Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    }
+ public void getEnchantress(View view){
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    } public void getEnchantress(View view){
+
+Intent intent = new Intent(PowerActivity.this,  .class);
+        startActivity(intent);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
