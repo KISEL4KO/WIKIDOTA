99fc2c16c45cbd399e19d0ebaec99115cd6b9180
detomboychik <egor2006.04.09@mail.ru>
2023-05-08 13:38:55 +0500

    statistics screen

---
+++ app/src/main/java/com/example/wikidota/Builder.java
@@ -0,0 +1,126 @@
+package com.example.wikidota;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringJoiner;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class Builder {
+
+    public static void items(String hero, Document html) {
+        try {
+            String[] heroName = hero.toLowerCase().split(" ");
+            StringJoiner joinedName = new StringJoiner("-");
+            for (String e: heroName){
+                joinedName.add(e);
+            }
+            String title = html.title();
+            System.out.println(title + '\n');
+            System.out.println("Top-12 most used items this week:" + "\n");
+            System.out.println("Item - Matches - Wins - Winrate\n");
+            Elements listItems = html.select("a[href]");
+            for (Element link : listItems) {
+                String linkText = link.text();
+                String linkHref = link.attr("href");
+                if (!linkText.equals("") &&
+                        linkHref.contains("/items/") &&
+                        !linkHref.contains("winning") &&
+                        !linkHref.contains("economy") && !linkHref.contains("impact")){
+                    String ask = "tr[data-link-to=" + linkHref + "] > td";
+                    String[] params = html.select(ask).text().split(" ");
+                    String[] slice = Arrays.copyOfRange(params, params.length - 3, params.length);
+                    StringJoiner joined = new StringJoiner(" - ");
+                    for (String e: slice){
+                        joined.add(e);
+                    }
+                    System.out.println(linkText + " - " + joined);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static boolean check(String[] arr, String name) {
+        for (String id : arr) {
+            if (name.equals(id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static ArrayList<String[]> statisticWins(Document html) {
+        Elements listsecs = html.select("section.home-hero-pulse").select("a.link-type-hero");
+        String[] listWinHeroesNames = new String[6];
+        ArrayList<String[]> listWinHeroes = new ArrayList<>();
+        int pos = 0;
+        for (Element e: listsecs) {
+            String name = e.text();
+            if (!check(listWinHeroesNames, name)) {
+                String[] forEveryHero = new String[3];
+                forEveryHero[0] = name;
+                Elements rates = html.select("tr[data-link-to=" + e.attr("href") + "] > td");
+                int i = 0;
+                for (Element k: rates){
+                    k.select("i");
+                    if (i != 0 && i != 1) {
+                        if (i == 3){
+                            if (k.select("i").toString().contains("Increased")) forEveryHero[i - 1] = '+' + k.text();
+                            else forEveryHero[i - 1] = '-' + k.text();
+                        }
+                        else {
+                            forEveryHero[i - 1] = k.text();
+                        }
+                    }
+                    i++;
+                }
+                listWinHeroes.add(forEveryHero);
+                listWinHeroesNames[pos] = name;
+                if (pos < 5) pos++;
+                else break;
+            }
+        }
+        return listWinHeroes;
+        // НУЖНО ПОМНИТЬ, ЧТО ПРИ СОЗДАНИИ ТАБЛИЦЫ НА ГЛАВНОМ ЭКРАНЕ НЕОБХОДИМО ОТСЕИВАТЬ NULL-СТРОКИ !!!
+    }
+    public static ArrayList<String[]> statisticsPicks(Document html) {
+        Elements listsecs = html.select("section.home-hero-pulse").select("a.link-type-hero");
+        String[] listPickHeroesNames = new String[6];
+        ArrayList<String[]> listPickHeroes = new ArrayList<>();
+        int poss = 0;
+        for (Element e: listsecs) {
+            if (poss >= listsecs.size() / 2) {
+                String name = e.text();
+                if (check(listPickHeroesNames, name)) {
+                    listPickHeroesNames[poss - listsecs.size()/2 - 1] = null;
+                }
+                String[] forEveryHeroP = new String[3];
+                forEveryHeroP[0] = name;
+                Elements rates = html.select("tr[data-link-to=" + e.attr("href") + "] > td");
+                int i = 0;
+                for (Element k : rates) {
+                    if (i != 0 && i != 1) {
+                        if (i == 3){
+                            if (k.select("i").toString().contains("Increased")) forEveryHeroP[i - 1] = '+' + k.text();
+                            else forEveryHeroP[i - 1] = '-' + k.text();
+                        }
+                        else {
+                            forEveryHeroP[i - 1] = k.text();
+                        }
+                    }
+                    i++;
+                }
+                listPickHeroes.add(forEveryHeroP);
+                listPickHeroesNames[poss - listsecs.size()/2] = name;
+                if (poss == listsecs.size()-1) {
+                    break;
+                }
+            }
+            poss++;
+        }
+        return listPickHeroes;
+    }
+}
--- app/src/main/java/com/example/wikidota/MainActivity.java
+++ app/src/main/java/com/example/wikidota/MainActivity.java
@@ -2,27 +2,131 @@ package com.example.wikidota;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+//    public TextView hero1, hero11, hero111, hero2, hero22, hero222, hero3, hero33, hero333, hero4, hero44, hero444;
+//    public TextView heroes1, heroes11, heroes111, heroes2, heroes22, heroes222, heroes3, heroes33, heroes333, heroes4, heroes44, heroes444;
+    public String request;
+    public String response;
+    public HttpsURLConnection connection;
+    public Document html;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        try {
+            html = new MyParser().execute("https://www.dotabuff.com").get();
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String[] finalList = new String[24];
+        int pos = 0;
+        for (String[] e : Builder.statisticWins(html)) {
+            for (String k : e) {
+                if (k != null) {
+                    finalList[pos] = k;
+                    pos++;
+                    System.out.println(k);
+                }
+                if (pos == 12) break;
+            }
+            if (pos == 12) break;
+        }
+        int poss = 12;
+        for (String[] e : Builder.statisticsPicks(html)) {
+            for (String k : e) {
+                if (k != null) {
+                    finalList[poss] = k;
+                    poss++;
+                    System.out.println(k);
+                }
+                if (poss == 24) break;
+            }
+            if (poss == 24) break;
+        }
+        int convenientPos = 0;
+        for (int i = 1; i < 3; i++) {
+            String base;
+            if (i == 1) {base = "hero";}
+            else {base = "heroes";}
+            for (int j = 1; j < 5; j++) {
+                for (int k = 1; k < 4; k++) {
+                    String bebra = "bebra!!!";
+                    switch (k){
+                        case 1:
+                            bebra = String.valueOf(j);
+                            break;
+                        case 2:
+                            bebra = String.valueOf(j) + String.valueOf(j);
+                            break;
+                        case 3:
+                            bebra = String.valueOf(j) + String.valueOf(j) + String.valueOf(j);
+                            break;
+                    }
+                    TextView view = findViewById(getResources().getIdentifier(base + bebra, "id", getPackageName()));
+                    view.setText(finalList[convenientPos]);
+                    convenientPos++;
+                }
+            }
+        }
 
     }
-public void getCharacher(View view){
-        Intent intent = new Intent(MainActivity.this ,  Character.class);
+
+    public void getCharacher(View view) {
+        Intent intent = new Intent(MainActivity.this, Character.class);
         startActivity(intent);
     }
-  public void getItem(View view){
 
-        Intent intent = new Intent(MainActivity.this ,  Items.class);
+    public void getItem(View view) {
+
+        Intent intent = new Intent(MainActivity.this, Items.class);
         startActivity(intent);
+
+    }
+    public class MyParser extends AsyncTask<String, Void, Document> {
+        @Override
+        protected Document doInBackground(String... links) {
+            Document htmll = null;
+            try {
+                request = links[0];
+                URL url = new URL(request);
+                connection = (HttpsURLConnection) url.openConnection();
+                connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                String line = "";
+                StringBuffer buffer = new StringBuffer();
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append("\n");
+                }
+                response = buffer.toString();
+                htmll = Jsoup.parse(response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return htmll;
+        }
 
     }
 }
